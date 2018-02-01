977efef4fa58eaa0420d0813e638380e3d358bce
sputterfish <sputterfish@gmail.com>
2018-02-01 10:38:47 -0700

    split cfgs to separate repos

--- SilentHunterFolderReader/Program.cs
+++ SilentHunterFolderReader/Program.cs
@@ -59,22 +59,6 @@ namespace SilentHunterFolderReader
                                 writer.WriteLine(line11);
                                 writer.WriteLine(line12);
                                 writer.Close();
-                            // OFEVcfg
-                            FileStream fs2 = new FileStream("OptionsFileEditorViewer.cfg", FileMode.OpenOrCreate, FileAccess.Write);
-                            StreamWriter writer2 = new StreamWriter(fs2);
-                            string newline1 = @"SH5InstallPath=" + @location;
-                            string newline2 = @"LastMenuTXT=" + @location + @"\data\Menu\menu.txt";
-                            string newline3 = @"LastOptionsFilePath=" + @location + @"\data\Scripts\Menu\";
-                            string newline4 = @"ClientAreaWidth=1014";
-                            string newline5 = @"ClientAreaHeight=736";
-                            string newline6 = @"LanguagePackInUse=";
-                            writer2.WriteLine(newline1);
-                            writer2.WriteLine(newline2);
-                            writer2.WriteLine(newline3);
-                            writer2.WriteLine(newline4);
-                            writer2.WriteLine(newline5);
-                            writer2.WriteLine(newline6);
-                            writer2.Close();
                         }
                     }
                 }
